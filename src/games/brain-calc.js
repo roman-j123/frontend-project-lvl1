44d38fca971dd6503102f37a382fb09ce5e81adf
Roman Ismagilov <roman.j123@ya.ru>
2022-01-29 19:13:58 +0300

    Ref: Упростил switch case, убрал лишние export, вынес вопрос имени пользователя в движок игры

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,33 +1,22 @@
 import readlineSync from 'readline-sync';
 import { getRandomNumber } from '../index.js';
-// Функция случайного выражения
-const getRandomExpression = () => {
+
+// Функция проверки выражения
+const checkExpressionResult = (firstValue, secondValue, operand) => {
+  switch (operand) {
+    case '-':
+      return (firstValue - secondValue);
+    case '*':
+      return (firstValue * secondValue);
+    default:
+      return (firstValue + secondValue);
+  }
+};
+const brainCalcEngine = () => {
   const arrayOfOperands = ['+', '-', '*'];
   const getRandomOperand = arrayOfOperands[Math.floor(Math.random() * arrayOfOperands.length)];
   const getRandomFirstValue = getRandomNumber();
   const getRandomSecondValue = getRandomNumber();
-  return { getRandomFirstValue, getRandomSecondValue, getRandomOperand };
-};
-// Функция проверки выражения
-const checkExpressionResult = (firstValue, secondValue, operand) => {
-  let currentAnswer = 0;
-  switch (String(operand)) {
-    case '+':
-      currentAnswer = firstValue + secondValue;
-      break;
-    case '-':
-      currentAnswer = firstValue - secondValue;
-      break;
-    case '*':
-      currentAnswer = firstValue * secondValue;
-      break;
-    default:
-      break;
-  }
-  return currentAnswer;
-};
-const brainCalcEngine = () => {
-  const { getRandomFirstValue, getRandomSecondValue, getRandomOperand } = getRandomExpression();
   console.log(`Question: ${getRandomFirstValue} ${getRandomOperand} ${getRandomSecondValue}`);
   const userAnswer = readlineSync.questionInt('Answer: ');
   const currectAnswer = checkExpressionResult(
--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 import readlineSync from 'readline-sync';
 
-export const MIN_VALUE = 0;
-export const MAX_VALUE = 100;
-export const MAX_ROUNDS = 3;
-export const ROUND_COUNTER = 0;
+const MIN_VALUE = 0;
+const MAX_VALUE = 100;
+const MAX_ROUNDS = 3;
 
-// Приветствуем пользователя
-export const helloUser = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
 // Общий движок для всех игр
 export const gameEngine = (game, rules) => {
   console.log('Welcome to the Brain Games!');
-  const userName = helloUser();
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
   console.log(rules);
   for (let i = 0; i < MAX_ROUNDS; i += 1) {
     const { currectAnswer, userAnswer } = game();
-    if (currectAnswer === userAnswer) {
-      console.log('Correct!');
-    }
     if (currectAnswer !== userAnswer) {
       return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${currectAnswer}'. \n Let's try again, ${userName}!`);
     }
+    console.log('Correct!');
   }
   return console.log(`Congratulations, ${userName}!`);
 };
