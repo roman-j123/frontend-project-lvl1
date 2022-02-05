c6292145081440a4247bd71c92714d45c989b2a0
Roman Ismagilov <roman.j123@ya.ru>
2022-02-05 11:17:27 +0300

    Ref: brain-progression generate array

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,18 +1,23 @@
 import getRandomNumber from '../random.js';
 
+const generateProgressionLine = (start, step, progressionLength) => {
+  const progressionArray = [];
+  for (let i = start; progressionArray.length <= progressionLength; i += 1) {
+    const indexValue = start + step * i;
+    progressionArray.push(indexValue);
+  }
+  return progressionArray;
+};
+
 const brainProgressionEngine = () => {
-  const arrayOfNumbers = [];
   const lengthOfArray = getRandomNumber(5, 15);
   const stepOfProgression = getRandomNumber(1, 10);
-  let startPoint = getRandomNumber();
+  const startPoint = getRandomNumber(1, 15);
   const secretPoint = getRandomNumber(0, lengthOfArray);
-  for (let i = 0; arrayOfNumbers.length <= lengthOfArray; i += 1) {
-    arrayOfNumbers.push((startPoint += stepOfProgression));
-  }
-  const correctAnswer = arrayOfNumbers[secretPoint];
-  const questionArray = arrayOfNumbers;
-  questionArray.splice(secretPoint, 1, '..');
-  const question = `Question: ${questionArray.join(' ')}`;
+  const progression = generateProgressionLine(startPoint, stepOfProgression, lengthOfArray);
+  const correctAnswer = progression[secretPoint];
+  progression.splice(secretPoint, 1, '..');
+  const question = `Question: ${progression.join(' ')}`;
   return { correctAnswer, question };
 };
 export default brainProgressionEngine;
